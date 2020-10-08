a0a9c1aaff17d3dd47b2a3b846b527298ad86619
THL20ab <you@example.comyou@example.com>
2020-10-08 14:40:27 +0200

    Created init, run, stop and update methods in the game class. Created an InputHandler class that will assist in interacting with the player.

--- src/com/company/animalistic/main/Game.java
+++ src/com/company/animalistic/main/Game.java
@@ -1,5 +1,25 @@
 package com.company.animalistic.main;
 
 public class Game {
+    int numberOfRounds, numberOfPlayers;
 
+    public Game() {
+        init();
+    }
+
+    private void init() {
+
+    }
+
+    private void run() {
+
+    }
+
+    private void stop() {
+
+    }
+
+    private void update() {
+
+    }
 }
---
+++ src/com/company/animalistic/tool/InputHandler.java
@@ -0,0 +1,5 @@
+package com.company.animalistic.tool;
+
+public class InputHandler {
+
+}
