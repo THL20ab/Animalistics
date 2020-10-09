42c1ed11c8ba3057b3ca78f6ce7fb8dd5b67df5e
THL20ab <you@example.comyou@example.com>
2020-10-09 10:07:05 +0200

    Allowing the player to enter the number of players and number of rounds.

--- src/com/company/animalistic/main/Game.java
+++ src/com/company/animalistic/main/Game.java
@@ -1,14 +1,19 @@
 package com.company.animalistic.main;
 
+import com.company.animalistic.tool.InputHandler;
+
 public class Game {
-    int numberOfRounds, numberOfPlayers;
+    private InputHandler inputHandler;
+    private int numberOfPlayers, numberOfRounds;
 
     public Game() {
         init();
     }
 
     private void init() {
-
+        inputHandler = new InputHandler();
+        numberOfPlayers = inputHandler.promptInt("Enter the number of players\t(1-4)\t: ", 1, 4);
+        numberOfRounds = inputHandler.promptInt("Enter the number of rounds\t(5-30)\t: ", 5, 30);
     }
 
     private void run() {
