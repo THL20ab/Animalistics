0dec0889a26e81511834cdf12a2951363a73e93a
THL20ab <you@example.comyou@example.com>
2020-10-09 10:05:46 +0200

    Implemented the InputHandler class to make it easier handling user input. Supports clearing the terminal and asking the player to input a String or Integer. Also wrote the code for presenting a menu for future use.

--- src/com/company/animalistic/tool/InputHandler.java
+++ src/com/company/animalistic/tool/InputHandler.java
@@ -1,5 +1,36 @@
 package com.company.animalistic.tool;
 
-public class InputHandler {
+import java.util.Scanner;
 
+public class InputHandler {
+    private final Scanner userInput = new Scanner(System.in);
+
+    public void clearTerminal() {
+        System.out.println("\n".repeat(100));
+    }
+
+    public String prompt(String message) { System.out.print(message); return userInput.nextLine(); }
+
+    public int promptInt(String message, int min, int max) {
+        //Set initial value to be out of scope
+        int number = min - 1;
+        //Try to capture the Integer
+        try {
+            number = Integer.parseInt(prompt(message));
+        } catch(Exception e) {
+            System.out.println("\nYou have to enter a number between " + min + " and " + max + ".\n");
+        }
+        //If value is out of scope prompt again, if not return the value
+        return number < min || number > max ? promptInt(message, min, max) : number;
+    }
+
+    public int menu(String[] options) {
+        clearTerminal();
+
+        for(int i = 0; i < options.length; i++) { System.out.println((i + 1) + ". " + options[i]); }
+
+        System.out.print("\n");
+
+        return promptInt("Choice: ", 1, options.length + 1);
+    }
 }
